0e10f280145aa427f5447823f29cee2541465347
Debug-Lee <2543520002@qq.com>
2024-04-07 15:40:00 +0800

    gitStudy get set Change

--- src/main/java/com/myblog/git/gitStudy.java
+++ src/main/java/com/myblog/git/gitStudy.java
@@ -1,22 +1,8 @@
 package com.myblog.git;
 
+import org.aspectj.weaver.ast.Or;
+
 public class gitStudy {
     private int id;
     private int age;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
