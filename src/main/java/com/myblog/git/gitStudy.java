2d025b36f1c46146362e38063685f32baa68e125
Debug-Lee <2543520002@qq.com>
2024-04-07 15:31:58 +0800

    gitStudy Change2

--- src/main/java/com/myblog/git/gitStudy.java
+++ src/main/java/com/myblog/git/gitStudy.java
@@ -1,4 +1,13 @@
 package com.myblog.git;
 
 public class gitStudy {
+    private int id;
+    private int age;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
