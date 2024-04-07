84842e2e7b956c3a79fd9ccb6b8126fb149aa2aa
Debug-Lee <2543520002@qq.com>
2024-04-07 15:34:12 +0800

    gitStudy get set Change

--- src/main/java/com/myblog/git/gitStudy.java
+++ src/main/java/com/myblog/git/gitStudy.java
@@ -3,11 +3,20 @@ package com.myblog.git;
 public class gitStudy {
     private int id;
     private int age;
+
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
